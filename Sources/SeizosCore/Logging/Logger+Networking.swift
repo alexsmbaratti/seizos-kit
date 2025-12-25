d90da19d06703611e4d17e0a7733011408ee6b6a
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2025-12-24 22:55:42 -0800

    Adding Logger

---
+++ Sources/SeizosCore/Logging/Logger+Networking.swift
@@ -0,0 +1,47 @@
+//
+//  Logger+Networking.swift
+//  SeizosKit
+//
+//  Created by Alex Baratti on 12/24/25.
+//
+
+import Foundation
+import OSLog
+
+public extension Logger {
+    
+    /// Logs a network request in a standardized format.
+    func logRequest(_ request: URLRequest) {
+        if let url = request.url {
+            debug(
+                "\(request.httpMethod ?? "Unknown method") \(url.absoluteString, privacy: .public)"
+            )
+        } else {
+            error("Failed to retrieve URL from request")
+        }
+    }
+    
+    /// Logs raw network response data.
+    func logResponse(_ data: Data) {
+        if let responseString = String(data: data, encoding: .utf8) {
+            debug("Response: \(responseString, privacy: .sensitive)")
+        } else {
+            error("Failed to convert response data to string")
+        }
+    }
+    
+    /// Logs a network error and optional HTTP response for a failed network request.
+    func logRequestError(response: URLResponse?, requestError: Error?) {
+        if let requestError {
+            error(
+                "Network request failed with error: \(requestError.localizedDescription, privacy: .sensitive)"
+            )
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                error("HTTP Status Code: \(httpResponse.statusCode)")
+            }
+        } else {
+            error("Unknown network error")
+        }
+    }
+}
