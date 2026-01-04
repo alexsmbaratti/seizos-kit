137067c3adb0c62beeb1e37dac517893e07671e9
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2026-01-03 23:31:15 -0800

    Refactoring for more parameters

--- Sources/SeizosUI/Views/StarRatingView.swift
+++ Sources/SeizosUI/Views/StarRatingView.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct StarRatingView: View {
-    let rating: Int8?
-    let fillColor: Color? = .yellow
+    @Binding private var rating: Int
+    private let maxRating: Int
+    private let fillColor: Color
+    private let isEditable: Bool
     
-    var body: some View {
-        HStack(spacing: 0) {
-            ForEach(1...5, id: \.self) { index in
-                Image(systemName: index <= rating ?? 0 ? "star.fill" : "star")
-                    .foregroundColor(index <= rating ?? 0 ? fillColor : .gray)
+    public init(
+            rating: Binding<Int>,
+            maxRating: Int = 5,
+            fillColor: Color = .yellow,
+            isEditable: Bool = true
+        ) {
+            self._rating = rating
+            self.maxRating = maxRating
+            self.fillColor = fillColor
+            self.isEditable = isEditable
+        }
+    
+    public var body: some View {
+            HStack(spacing: 0) {
+                ForEach(1...maxRating, id: \.self) { index in
+                    Image(systemName: index <= rating ? "star.fill" : "star")
+                        .foregroundStyle(index <= rating ? fillColor : .gray)
+                        .onTapGesture {
+                            guard isEditable else { return }
+                            rating = index
+                        }
+                }
             }
         }
-    }
 }
 
 #Preview {
-    StarRatingView(rating: 3)
+    StarRatingView(rating: .constant(3))
 }
