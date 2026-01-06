777607673b6d3fc959d5909ac524e6c5f1a3e309
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2026-01-05 22:37:30 -0800

    Adding accessibility parameters

--- Sources/SeizosUI/Views/StarRatingView.swift
+++ Sources/SeizosUI/Views/StarRatingView.swift
@@ -7,22 +7,39 @@
 
 import SwiftUI
 
+/// A declarative view for displaying a star rating system, with customizations for fill color and number of stars.
+///
+/// - Note:
+/// For full accessibility and localization, consumers should provide localized accessibility labels and values.
+///
+/// - Parameter rating: A binding to the number of filled stars. Pass a constant binding if the view is intended to be read-only.
+/// - Parameter maxRating: The maximum number of stars possible. For example, for a five-star rating system, the `maxRating` would be `5`.
+/// - Parameter fillColor: The color of filled stars. Defaults to yellow.
+/// - Parameter isEditable: Whether the rating is dynamic.
+/// - Parameter accessibilityLabel: A localized string key representing the label for this view (i.e. "Rating").
+/// - Parameter accessibilityValue: A localized string key representing the value for this view (i.e. "3 out of 5 stars"). Used for VoiceOver. This should incorporate `rating` and `maxRating`.
 public struct StarRatingView: View {
     @Binding private var rating: Int
     private let maxRating: Int
     private let fillColor: Color
     private let isEditable: Bool
+    private let accessibilityLabel: LocalizedStringKey
+    private let accessibilityValue: LocalizedStringKey
     
     public init(
             rating: Binding<Int>,
             maxRating: Int = 5,
             fillColor: Color = .yellow,
-            isEditable: Bool = true
+            isEditable: Bool = true,
+            accessibilityLabel: LocalizedStringKey = "",
+            accessibilityValue: LocalizedStringKey = ""
         ) {
             self._rating = rating
             self.maxRating = maxRating
             self.fillColor = fillColor
             self.isEditable = isEditable
+            self.accessibilityLabel = accessibilityLabel
+            self.accessibilityValue = accessibilityValue
         }
     
     public var body: some View {
@@ -36,8 +53,8 @@ public struct StarRatingView: View {
                         }
                 }
             }
-            .accessibilityLabel("Rating")
-            .accessibilityValue("\(rating) out of \(maxRating) stars")
+            .accessibilityLabel(accessibilityLabel)
+            .accessibilityValue(accessibilityValue)
             .accessibilityAdjustableAction { direction in
                 guard isEditable else { return }
                 
